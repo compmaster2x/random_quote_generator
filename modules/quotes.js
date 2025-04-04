f0d5a93eea04ffb224660ec180e90eb1da78b084
compmaster2x <111523497+compmaster2x@users.noreply.github.com>
2025-04-04 12:23:51 +0300

    changes for 1.0.0 version of program, with bug in function generateRandomQuote

--- modules/quotes.js
+++ modules/quotes.js
@@ -2,42 +2,52 @@ const quotes = [
     {
       text: 'The only way to do great work is to love what you do.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'Innovation distinguishes between a leader and a follower.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: "Your time is limited, so don't waste it living someone else's life.",
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'Stay hungry, stay foolish.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'The people who are crazy enough to think they can change the world are the ones who do.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'Sometimes life hits you in the head with a brick. Don’t lose faith.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'I’m convinced that about half of what separates successful entrepreneurs from the non-successful ones is pure perseverance.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'Great things in business are never done by one person. They’re done by a team of people.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'Be a yardstick of quality. Some people aren’t used to an environment where excellence is expected.',
       author: 'Steve Jobs',
+      isFavorite: false,
     },
     {
       text: 'My favorite things in life don’t cost any money. It’s really clear that the most precious resource we all have is time.',
       author: 'Steve Jobs',
+      isFavorite: false,
     }
   ];
 
--- script.js
+++ script.js
@@ -3,14 +3,24 @@ import quotes from "./modules/quotes.js";
   
   const quoteElement = document.getElementById('quote');
   const generateBtn = document.getElementById('generate-btn');
-  
-  function generateRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
-    quoteElement.innerHTML = `${randomQuote.text}  <br>  ${randomQuote.author}` ;
-    
+  const quoteAuthorElement = document.getElementById('quote-author')
+  const toggleFavoriteBtn = document.getElementById('make-fav')
 
+  let currentQuoteIndex
+
+
+  function generateRandomQuote() {
+    currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[currentQuoteIndex];
+    const {quote, author}  = randomQuote
+    quoteElement.textContent = quote
+    quoteAuthorElement.textContent = author
+  }
+
+  function toggleFavorite(){
+    const currentQuote= quotes[currentQuoteIndex]
+    currentQuote.isFavorite = !currentQuote.isFavorite
   }
   
   generateBtn.addEventListener('click', generateRandomQuote);
-  
+  toggleFavoriteBtn.addEventListener('click', toggleFavorite)
